8d0d6938f1d5d18ef464aa34a8f6ed92c8fd13ad
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2022-05-01 14:42:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+cookies=document.getElementById("random");
+
+Webcam.attach('#random');
+
+function brownie(){
+    Webcam.snap(
+        function(data_uri){
+            document.getElementById("random2.0").innerHTML='<img id="random3.0" src="'+data_uri+'"/>';
+        }
+    );
+}
+
+console.log('This is ml5',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NbcUZCcE2/',modelLoaded);
+
+function modelLoaded(){
+ console.log('Family');
+}
+
+function cakepop(){
+    myimg=document.getElementById('random3.0');
+    classifier.classify(myimg,gotresult);
+}
+
+function gotresult(error,results){
+     if(error){
+         console.error(error);
+     }
+     else{
+         console.log(results);
+         document.getElementById("object_name").innerHTML=results[0].label;
+         document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+     }
+}
